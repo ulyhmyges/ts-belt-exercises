ee8f6837ccdfba8aa26515438269362583e911c5
ulyh <janeuly@pm.me>
2024-09-12 17:26:08 +0200

    ArrayFn10 works

--- src/array/ex10.ts
+++ src/array/ex10.ts
@@ -1,10 +1,20 @@
-// // Exercice 10: Insérer des produits à des positions spécifiquesUtilise Array.insertAt (pour insérer un produit à l'indice 1).Utilise Array.prependToAll (pour ajouter un produit à chaque élément d'une liste).Utilise Array.append (pour ajouter un produit à la fin de la liste)
+// // Exercice 10: Insérer des produits à des positions spécifiques
+// Utilise Array.insertAt(pour insérer un produit à l'indice 1).
+// Utilise Array.prependToAll(pour ajouter un produit à chaque élément d'une liste).
+// Utilise Array.append(pour ajouter un produit à la fin de la liste)
 
-// import { Product } from "./type";
+import { Product } from "./type";
+import { A, O, pipe } from "@mobily/ts-belt";
 
-// const products: Product[] = [
-//   { name: 'Laptop', price: 1000, inStock: true },
-//   { name: 'Mouse', price: 25, inStock: false },
-//   { name: 'Keyboard', price: 75, inStock: true },
-// ];
-export const ArrayFn10 = () => {};
+export const products: Product[] = [
+  { name: "Laptop", price: 1000, inStock: true },
+  { name: "Mouse", price: 25, inStock: false },
+  { name: "Keyboard", price: 75, inStock: true },
+];
+export const ArrayFn10 = <T>(arr: T[], index: number, value: T, delimiter: T) =>
+  pipe(
+    arr,
+    A.insertAt(index, value),
+    A.prependToAll(delimiter),
+    A.append(value)
+  );
--- src/test/array/ArrayFn10.test.ts
+++ src/test/array/ArrayFn10.test.ts
@@ -1,18 +1,54 @@
+import { products } from "../../array/ex1";
+import { ArrayFn10 } from "../../array/ex10";
+
 describe("ArrayFn10 Tests", () => {
   test("Check ArrayFn10 function", () => {
-    const result = ArrayFn10(inventory1, inventory2);
+    const value = { name: "insert_value", price: 10, inStock: true };
+    const index = 3;
+    const delimiter = { name: "delimiter", price: 112, inStock: true };
+    const result = ArrayFn10(products, index, value, delimiter);
     expect(result).toStrictEqual([
-      [
-        { name: "Laptop", price: 1000, inStock: true },
-        { name: "Mouse", price: 25, inStock: false },
-      ],
-      [{ name: "Keyboard", price: 75, inStock: true }],
-      [
-        { name: "Laptop", price: 1000, inStock: true },
-        { name: "Mouse", price: 25, inStock: false },
-        { name: "Keyboard", price: 75, inStock: true },
-        { name: "Monitor", price: 200, inStock: false },
-      ],
+      {
+        inStock: true,
+        name: "delimiter",
+        price: 112,
+      },
+      {
+        inStock: true,
+        name: "Laptop",
+        price: 1000,
+      },
+      {
+        inStock: true,
+        name: "delimiter",
+        price: 112,
+      },
+      {
+        inStock: false,
+        name: "Mouse",
+        price: 25,
+      },
+      {
+        inStock: true,
+        name: "delimiter",
+        price: 112,
+      },
+      {
+        inStock: true,
+        name: "Keyboard",
+        price: 75,
+      },
+      {
+        inStock: true,
+        name: "delimiter",
+        price: 112,
+      },
+      {
+        inStock: true,
+        name: "insert_value",
+        price: 10,
+      },
+      { inStock: true, name: "insert_value", price: 10 },
     ]);
   });
 });
