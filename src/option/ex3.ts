d16cc2bb987cf5e64eae447f22a9f0b6a5672d37
ulyh <janeuly@pm.me>
2024-09-13 09:34:59 +0200

    optionFn3 works

--- src/option/ex3.ts
+++ src/option/ex3.ts
@@ -1,5 +1,19 @@
-// // Exercice 3: Associer deux options et appliquer une fonctionFonctions à utiliser : fromNullable, zipWith, mapObjectif : Associer deux options, puis utiliser une fonction pour ajouter les deux valeurs ensemble, et enfin transformer le résultat pour afficher une chaîne.
+// // Exercice 3: Associer deux options et appliquer une fonction
+// Fonctions à utiliser: fromNullable, zipWith, map
+// Objectif: Associer deux options, puis utiliser une fonction pour ajouter les deux valeurs ensemble,
+// et enfin transformer le résultat pour afficher une chaîne.
 
-// const value1 = 100;
-// const value2 = 50;
-export const optionFn3 = () => {};
+import { A, O, pipe } from "@mobily/ts-belt";
+
+export const value1 = 100;
+export const value2 = 50;
+export const optionFn3 = (val1: number, val2: number) =>
+  pipe(
+    O.fromNullable(val1),
+    O.zipWith(O.fromNullable(val2), (arg1: number, arg2: number): number[] => [
+      arg1,
+      arg2,
+    ]),
+    O.map((n) => `${n[0]}.${n[1]}`),
+    O.getWithDefault(String(""))
+  );
--- src/test/option/optionFn3.test.ts
+++ src/test/option/optionFn3.test.ts
@@ -1,6 +1,8 @@
-// describe("optionFn1 Tests", () => {
-//   test("Check ArrayFn1 function", () => {
-//     const result = optionFn1();
-//     expect(result).toStrictEqual();
-//   });
-// });
+import { optionFn3, value1, value2 } from "../../option/ex3";
+
+describe("optionFn3 Tests", () => {
+  test("Check optionFn3 function", () => {
+    const result = optionFn3(value1, value2);
+    expect(result).toStrictEqual(`${value1}.${value2}`);
+  });
+});
