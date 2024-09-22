95c8d93f00f92747eb0ec74bcdf234a1b3c14b90
ulyh <janeuly@pm.me>
2024-09-22 09:14:48 +0200

    resultFn1 works - exception

--- src/result/ex1.ts
+++ src/result/ex1.ts
@@ -1,9 +1,20 @@
-// // Exercice 1: Transformer une exécution en Result, appliquer une transformation, puis obtenir la valeur par défautFonctions à utiliser : fromExecution, map, getWithDefaultObjectif : Transformer une exécution en Result, appliquer une transformation qui multiplie la valeur par 2, puis récupérer une valeur par défaut si l'exécution échoue.
+// // Exercice 1: Transformer une exécution en Result,
+// appliquer une transformation, puis obtenir la valeur par défaut
+// Fonctions à utiliser: fromExecution, map, getWithDefault
+// Objectif: Transformer une exécution en Result, appliquer une transformation qui multiplie la valeur par 2, puis récupérer une valeur par défaut si l'exécution échoue.
 
-// const riskyFunction = () => {
-//   if (Math.random() > 0.5) {
-//     return 100;
-//   }
-//   throw new Error('Erreur');
-// };
-export const resultFn1 = () => {};
+import { R, pipe, O } from "@mobily/ts-belt";
+
+const riskyFunction = () => {
+  if (Math.random() > 0.5) {
+    return 100;
+  }
+  throw new Error("Erreur");
+};
+export const resultFn1 = () => {
+  return pipe(
+    R.fromExecution(riskyFunction),
+    R.map((x) => x * 2),
+    R.getWithDefault(Number(-1))
+  );
+};
---
+++ src/test/result/resultFn1.test.ts
@@ -0,0 +1,11 @@
+import { resultFn1 } from "../../result/ex1";
+
+describe("resultFn1 Tests", () => {
+  test("Check resultFn1 function", () => {
+    // const result = resultFn1();
+    // without exception
+    // expect(result).toStrictEqual(100 * 2);
+    // with exception
+    // expect(result).toStrictEqual(-1);
+  });
+});
