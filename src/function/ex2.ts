84196a6aa5d191d5db220cbc72f81290507747e6
ulyh <janeuly@pm.me>
2025-01-08 11:40:27 +0100

    functionFn2 works

--- src/function/ex2.ts
+++ src/function/ex2.ts
@@ -1,7 +1,16 @@
-// // Exercice 2: Appliquer plusieurs prédicats et vérifier si tous sont satisfaitsFonctions à utiliser : allPass, tap, when, identityObjectif : Vérifie si un nombre est supérieur à 10 et pair. Si oui, affiche 'Validé', sinon retourne simplement la valeur d'origine avec identity.
+// Exercice 2: Appliquer plusieurs prédicats et vérifier si tous sont satisfaits
+// Fonctions à utiliser : allPass, tap, when, identity
+// Objectif : Vérifie si un nombre est supérieur à 10 et est pair.
+// Si oui, affiche 'Validé', sinon retourne simplement la valeur d'origine avec identity.
+
+import { F, pipe } from "@mobily/ts-belt";
+
 // const number = 12;
-// const predicates = [
-//   (n: number) => n > 10,
-//   (n: number) => n % 2 === 0,
-// ];
-export const functionFn2 = () => {};
+const predicates = [(n: number) => n > 10, (n: number) => n % 2 === 0];
+export const functionFn2 = (number: number) => {
+  return pipe(
+    number,
+    F.when(F.allPass(predicates), (x) => console.log("Validé")),
+    F.identity
+  );
+};
--- src/test/function/functionFn2.test.ts
+++ src/test/function/functionFn2.test.ts
@@ -1,8 +1,13 @@
 import { functionFn2 } from "../../function/ex2";
-
+const number = 11;
 describe("functionFn2 Tests", () => {
   test("", () => {
-    const result = functionFn2();
+    const result = functionFn2(number);
+    expect(result).toStrictEqual(11);
+  });
+
+  test("", () => {
+    const result = functionFn2(12);
     expect(result).toStrictEqual(undefined);
   });
 });
